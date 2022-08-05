6ad14fe11714faf4bbab32cf8586c81688e54e83
Nikita <s2015836@x.donstu.ru>
2022-08-05 19:20:26 +0300

    Add stop command

---
+++ src/commands/stop.js
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from 'discord.js'
+
+export const data = new SlashCommandBuilder()
+    .setName('stop')
+    .setDescription('Выключить музыку')
+
+export const execute = async interaction => {
+    if (interaction.client.player) {
+        interaction.client.player.disconnect()
+        interaction.reply(`${interaction.user.username} выключил воспроизведение`)
+    } else {
+        interaction.reply({content: 'Ничего не включено', ephemeral: true})
+    }
+}
